6e6052463a806dbdb72a48a88e8a44f33e523c0a
dataneo <bprv6qazvbwni@gmail.com>
2018-06-25 00:56:57 +0200

    Work on MainPage.xaml

---
+++ FreeDiscDownloader/FreeDiscDownloader/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeDiscDownloader
+{
+    class Item
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Size { get; set; }
+        public string Autor { get; set; }
+        public string TypeImage { get; set; }
+        public string Date { get; set; }
+    }
+}
--- FreeDiscDownloader/FreeDiscDownloader/MainPage.xaml.cs
+++ FreeDiscDownloader/FreeDiscDownloader/MainPage.xaml.cs
@@ -11,7 +11,46 @@ namespace FreeDiscDownloader
 	{
 		public MainPage()
 		{
+            
 			InitializeComponent();
-		}
+            listItem.ItemsSource = ExampleList();
+
+        }
+
+        private static IEnumerable<Item> ExampleList()
+        {
+            return new List<Item>()
+            {
+                new Item()
+                {
+                    Title = "MICROSOFT_PRESS_EBOOK_XAMARIN_PREVIEW_2_PDF.PDF",
+                    Image = @"‪https://placeimg.com/100/100/people/2",
+                    Size = "Rozmiar: 14.7 MB",
+                    Autor = "Dodał: achromski ",
+                    Date = "02 sie 17 21:53",
+                    TypeImage = "A",
+                },
+                new Item()
+                {
+                    Title = "MICROSOFT_PRESS_EBOOK_XAMARIN_PREVIEW_2_PDF.PDF",
+                    Image = @"‪https://placeimg.com/100/100/people/2",
+                    Size = "Rozmiar: 14.7 MB",
+                    Autor = "Dodał: achromski ",
+                    Date = "02 sie 17 21:53",
+                    TypeImage = "A",
+                },
+                new Item()
+                {
+                    Title = "MICROSOFT_PRESS_EBOOK_XAMARIN_PREVIEW_2_PDF.PDF",
+                    Image = @"‪https://placeimg.com/100/100/people/2",
+                    Size = "Rozmiar: 14.7 MB",
+                    Autor = "Dodał: achromski ",
+                    Date = "02 sie 17 21:53",
+                    TypeImage = "A",
+                },
+            };
+
+        }
+
 	}
 }
